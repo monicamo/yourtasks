04deaf1279c7431056becb376594985c4ebe5852
Monica Monteiro <1221959+monicamo@users.noreply.github.com>
2025-02-24 16:32:55 -0300

    Estado void

---
+++ src/app/animations.ts
@@ -0,0 +1,37 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+export const highlightedStateTrigger = trigger('highlightedState', [
+    state('default', style({
+      border: '2px solid #B2B6FF',
+    })),
+    state('highlighted', style({
+      border: '4px solid #B2B6FF',
+      filter:'brightness(92%)'
+    })),
+    transition('default => highlighted', [
+      animate('200ms ease-out',  style({
+        transform: 'scale(1.02)'
+      })),
+      animate(200)
+    ])
+  ])
+
+
+  export const showStateTrigger = trigger('showState', [
+    state('shown', style({
+
+    })),
+    transition('void => shown', [
+      style({
+        opacity: 0
+      }),
+      animate(300, style({
+        opacity: 1
+      }))
+    ]),
+    transition('shown => void', [
+      animate(300, style({
+        opacity: 0
+      }))
+    ])
+  ])
